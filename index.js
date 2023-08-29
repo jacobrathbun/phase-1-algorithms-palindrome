8fbeb4331296e30bbfdd51648a714656df144b67
Jacob Rathbun <jrathofbun@gmail.com>
2023-08-29 15:17:59 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  psuedocode
+  isPalindrome(word)
+    reversedWord = word.reverse.join
+    rtrn word === reversedWord
 */
 
 /*
-  Add written explanation of your solution here
+  explanation
+  Original word is passed into isPalindrome function
+  word is then reversed into a new string using .split.reverse.join
+  original string is compared to new reversed string
+  output true if equal
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
